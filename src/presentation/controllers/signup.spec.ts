cbc6bf744579db6963f539015a055c5ba1cc7b20
Raphael <oraphaeldemelo@outlook.com>
2024-04-24 09:22:34 -0300

    change mocking stategy

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,14 +7,18 @@ interface SutTypes {
   emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
       return true
     }
   }
-
   const emailValidatorStub = new EmailValidatorStub()
+  return emailValidatorStub
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
 
   return {
@@ -114,14 +118,11 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator', () => {
-    class EmailValidatorStub implements EmailValidator {
-      isValid (email: string): boolean {
-        throw new ServerError()
-      }
-    }
+    const { sut, emailValidatorStub } = makeSut()
 
-    const emailValidatorStub = new EmailValidatorStub()
-    const sut = new SignUpController(emailValidatorStub)
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new ServerError()
+    })
 
     const httpRequest = {
       body: {
