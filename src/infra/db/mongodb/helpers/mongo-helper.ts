4af74e75a3b85b0d9b1c8ebe7cf441bd930a6e48
Raphael <oraphaeldemelo@outlook.com>
2024-04-30 08:04:43 -0300

    ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,17 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+    client: null as MongoClient,
+
+    async connect (url: string): Promise<void> {
+        this.client = await MongoClient.connect(url)
+    },
+
+    async disconnect (): Promise<void> {
+        await this.client.close()
+    },
+
+    async getCollection (name: string): Promise<Collection> {
+        return this.client.db().collection(name)
+    }
+}
