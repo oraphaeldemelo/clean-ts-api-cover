93e5cf5d5806525cacf2cae81466f0cee4d65da2
Raphael <oraphaeldemelo@outlook.com>
2024-04-30 15:02:53 -0300

    connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,5 +1,20 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await (await accountCollection).deleteMany({})
+})
+
+afterAll(async () => {
+    await MongoHelper.disconnect()
+})
+
 
 describe('SignUp Middleware', () => {
     test('Should return an account on sucess', async () => {
