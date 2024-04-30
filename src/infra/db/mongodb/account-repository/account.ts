5895b78c45394461a8882b08de739b28b0f348b2
Raphael <oraphaeldemelo@outlook.com>
2024-04-30 08:53:41 -0300

    add express

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,8 +7,7 @@ export class AccountMongoRepository implements AddAccountRepository {
     async add (accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = MongoHelper.getCollection('accounts')
         const result = await (await accountCollection).insertOne(accountData)
-        const account = { id: result.insertedId.toString(), ...accountData }
 
-        return account
+        return { id: result.insertedId.toString(), ...accountData }
     }
 }
