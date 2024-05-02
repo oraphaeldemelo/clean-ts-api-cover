210783778e0f4867a8f717b67aed3a4b9765fc8e
Raphael <oraphaeldemelo@outlook.com>
2024-05-02 14:35:12 -0300

    ensure mongodb reconnect is client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,8 +5,8 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
     async add (accountData: AddAccountModel): Promise<AccountModel> {
-        const accountCollection = MongoHelper.getCollection('accounts')
-        const result = await (await accountCollection).insertOne(accountData)
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const result = await accountCollection.insertOne(accountData)
 
         return { id: result.insertedId.toString(), ...accountData }
     }
