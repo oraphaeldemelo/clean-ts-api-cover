6a162272fa17fc8c377f1879161318ac9b97ebef
Raphael <oraphaeldemelo@outlook.com>
2024-05-03 08:22:13 -0300

    ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,15 +1,28 @@
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { serverError } from '../../presentation/helper/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
 interface SutTypes {
     sut: LogControllerDecorator
     controllerStub: Controller
+    logErrorRepositoryStub: LogErrorRepository
+}
+
+const makeLogErrorRepository = (): LogErrorRepository => {
+    class LogErrorRepositoryStub implements LogErrorRepository {
+        async log (stackError: string): Promise<void> {
+            return new Promise(resolve => { resolve() })
+        }
+    }
+    return new LogErrorRepositoryStub()
 }
 
 const makeSut = (): SutTypes => {
     const controllerStub = makeController()
-    const sut = new LogControllerDecorator(controllerStub)
-    return { sut, controllerStub }
+    const logErrorRepositoryStub = makeLogErrorRepository()
+    const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub)
+    return { sut, controllerStub, logErrorRepositoryStub }
 }
 
 const makeController = (): Controller => {
@@ -17,7 +30,7 @@ const makeController = (): Controller => {
         async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
             const httpResponse = {
                 statusCode: 200,
-                body: { name: 'any_name' }
+                body: { name: 'Raphael' }
             }
             return new Promise((resolve) => { resolve(httpResponse) })
         }
@@ -42,5 +55,31 @@ describe('LogController Decorator', () => {
         const httpRequest = makeFakeRequest()
         await sut.handle(httpRequest)
         expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+    })
+
+    test('SHould return the same result of the controller', async () => {
+        const { sut } = makeSut()
+        const httpRequest = { body: { name: 'Raphael' } }
+        const httpResponse = await sut.handle(httpRequest)
+
+        expect(httpResponse).toEqual({
+            statusCode: 200,
+            body: {
+                name: 'Raphael'
+            }
+        })
+    })
+
+    test('Should call logErrorRepository with correct error if controller returns a server error', async () => {
+        const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
+        const fakeError = new Error()
+        fakeError.stack = 'any_stack'
+        const error = serverError(fakeError)
+        const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+        jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => { resolve(error) }))
+        const httpRequest = makeFakeRequest()
+        await sut.handle(httpRequest)
+
+        expect(logSpy).toHaveBeenCalledWith('any_stack')
     })
 })
--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,19 +1,22 @@
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
     private readonly controller: Controller
+    private readonly logErrorRepository: LogErrorRepository
 
-    constructor (controller: Controller) {
+    constructor (controller: Controller, logErrorRepository: LogErrorRepository) {
         this.controller = controller
+        this.logErrorRepository = logErrorRepository
     }
 
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-         await this.controller.handle(httpRequest)
+         const httpResponse = await this.controller.handle(httpRequest)
 
-        // if(httpResponse.statusCode === 500) {
+        if (httpResponse.statusCode === 500) {
+            await this.logErrorRepository.log(httpResponse.body.stack)
+        }
 
-        // }
-
-        return new Promise((resolve) => { resolve(null) })
+        return httpResponse
     }
 }
